0ec0a66c3aa0f67f27b719c5fdd83dcf7800cb4f
Marius Trica <m.trica@palmabit.com>
2022-04-15 20:48:07 +0200

    fix type

--- src/firebase/readNotification.ts
+++ src/firebase/readNotification.ts
@@ -1,10 +1,10 @@
-import { child, getDatabase, push, ref, set, update } from 'firebase/database';
-import _ from 'lodash';
-import firebaseApp from './initFirebase';
+import { child, getDatabase, push, ref, set, update } from "firebase/database";
+import _ from "lodash";
+import firebaseApp from "./initFirebase";
 
 const readNotification = (to: string, id: string) => {
   const db = getDatabase(firebaseApp());
-  const updates = {};
+  const updates: any = {};
   updates[`notifications/${to}/${id}/isRead`] = true;
   return update(ref(db), updates);
 };
